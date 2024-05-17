ef5e91b310c3f1cdc8818b742a33dc82c2120d62
pouyaniranpour <pouyan.iranpour@gmail.com>
2024-05-17 19:39:03 +1200

    changed api_host in countries file

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,19 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-const API_HOST = import.meta.env.VITE_APP_API_HOST;
+// const API_HOST = import.meta.env.VITE_APP_API_HOST;
+const API_HOST = 'https://mr-countries-names.azurewebsites.net/';
+
+console.log(API_HOST);
+
 
 export function Countries() {
   const { isPending, error, data, isFetching } = useQuery({
     queryKey: ["countries"],
     queryFn: () => axios.get(`${API_HOST}`).then((res) => res.data),
   });
-
+ 
   return (
     <div className="p-8">
       <h2 className="text-2xl font-bold">All Countries</h2>
       <div className="mt-4">
-        {/* 
-        // Try to get the data to load from the API
         {isFetching ? (
           <div className="text-blue-500">Fetching...</div>
         ) : isPending ? (
@@ -23,23 +25,14 @@ export function Countries() {
             An error has occurred: {error.message}
           </div>
         ) : (
-        <ul>
-          {Array.from({ length: 1000 }).map((_, index) => (
-            <li key={index}>
-              {data.map((country) => (
-                <li key={country.code}>
-                  {country.name} - {country.code}
-                </li>
-              ))}
-            </li>
-          ))}
-        </ul>
-        )} */}
-        {Array.from({ length: 1000 }).map((_, index) => (
-          <li key={index}>
-            <span>New Zealand / Aotearoa</span>
-          </li>
-        ))}
+          <ul>
+            {data.map((country) => (
+              <li key={country.Code}>
+                {country.Name} - {country.Code}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
